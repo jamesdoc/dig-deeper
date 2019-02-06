1dbb2a9e9bac0dfa85c6a7cd581e936e01295ad9
James Doc <hi@jamesdoc.com>
2019-02-06 09:40:17 +0100

    Improved navigation between states (#5)

--- src/app/study/study.page.ts
+++ src/app/study/study.page.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-study',
   templateUrl: './study.page.html',
   styleUrls: ['./study.page.scss'],
 })
+
 export class StudyPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  markAsComplete() {
+    console.log('TODO: Register study as complete');
+    this.router.navigateByUrl('/');
   }
 
 }
